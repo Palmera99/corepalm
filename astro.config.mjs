8bba246d0c945b490f46103cbf25eff91aa73e87
INVESTIGACIONES\hdiazb <hdiazb@investigaciones.cl>
2026-09-02 12:09:57 -0400

    Configurar despliegue automatico con GitHub Actions en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://corepalm.com',
+  site: 'https://palmera99.github.io',
+  base: '/corepalm',
   compressHTML: true,
   build: {
     format: 'directory'
